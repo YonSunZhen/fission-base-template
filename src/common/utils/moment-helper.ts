658d4fba17872517c96732e1f49015835f0f3ef8
Sun Yongzhen <1315976441@qq.com>
2021-06-29 21:08:33 +0800

    update moment-help class

--- src/common/utils/moment-helper.ts
+++ src/common/utils/moment-helper.ts
@@ -1,8 +1,21 @@
 import moment from 'moment-timezone';
+
+type MomentInput = moment.MomentInput;
+type MomentFormatSpecification = moment.MomentFormatSpecification;
 export class Moment {
 
-  setTz(tz = 'Asia/ShangHai') {
-    return moment().tz(tz);
+  inp: MomentInput;
+  format: MomentFormatSpecification;
+  constructor(inp?: MomentInput, format?: MomentFormatSpecification) {
+    this.inp = inp;
+    this.format = format;
   }
-
+  setTz(tz = 'Asia/ShangHai') {
+    if(this.inp) {
+      return moment(this.inp, this.format).tz(tz);
+    } else {
+      return moment().tz(tz);
+    }
+  }
+  
 }
